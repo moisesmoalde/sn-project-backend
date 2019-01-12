c0bfe4b4dff072860fae912b636090c01b3768a7
Lorena <lorenadmm@gmail.com>
2019-01-12 20:30:47 +0100

    Add files via upload

---
+++ apirest.py
@@ -0,0 +1,88 @@
+import cherrypy
+import random
+import string
+from py2neo import Graph, Node, Relationship
+import json
+import pandas as pd
+from Movie import Movie
+import User
+import similarity
+import neo4jUtils
+
+def error_page_404(status, message, traceback, version):
+    return "404 Error!"
+
+DB_USER = 'appi'
+DB_PWD = 'apirest'
+MOVIE_TYPE = 'Movie'
+USER_TYPE = 'User'
+
+GRAPH = Graph(user = DB_USER, password = DB_PWD)
+	
+class ApiRest():
+	
+	@cherrypy.expose
+	@cherrypy.tools.json_out()
+	@cherrypy.tools.json_in()
+	def AddUser (self, **kwargs):
+	
+		input_json = cherrypy.request.json
+		print(input_json)
+		email = input_json['email']
+		
+		fbID = input_json['facebook-id']
+		movies_likes = input_json['movies']
+		name = input_json['name']
+		surname = input_json['lastname']
+		neo4jUtils. insertUser(fbID, email, name, surname)
+		for i in movies_likes:
+			movieFBID = i['fbID']
+			r = neo4jUtils.insertEdge(name, movieFBID, 'LIKES')
+			
+		user = GRAPH.run('''
+				MATCH (user:User)-[:LIKES]-(movies) WHERE user.email = "{0}" RETURN user,movies;
+			   '''
+                .format(email)).data()	
+		return user
+		
+	@cherrypy.expose
+	@cherrypy.tools.json_out()
+	def GetUser(self, id):
+		user = GRAPH.run('''
+				MATCH (user:User)-[:LIKES]-(movies) WHERE user.name = "{0}" RETURN user,movies;
+			   '''
+                .format(id)).data()
+		return user
+	
+	@cherrypy.expose
+	@cherrypy.tools.json_out()
+	def MovieRecommendation(self, id, skip, limit):
+		movies = neo4jUtils.getRecommendedMovies(id,skip,limit)
+		print(movies)
+		return movies
+	
+	@cherrypy.expose
+	@cherrypy.tools.json_out()
+	@cherrypy.tools.json_in()
+	def AddMovie (self, email):
+		input_json = cherrypy.request.json
+		movies_likes = input_json['likes']
+		movies_dislikes = input_json['dislikes']
+		for i in movies_likes:
+			movieFBID = i['fbID']
+			r = neo4jUtils.insertEdge(email, movieFBID, 'LIKES')
+		user = GRAPH.run('''
+				MATCH (user:User)-[:LIKES]-(movies) WHERE user.email = "{0}" RETURN user,movies;
+			   '''
+                .format(email)).data()		
+		return user	
+
+
+def start_server():
+		cherrypy.tree.mount(ApiRest(), '/')
+		cherrypy.config.update({'error_page.404': error_page_404})
+		cherrypy.config.update({'server.socket_port': 8980})
+		cherrypy.engine.start()
+
+if __name__ == '__main__':
+    start_server()
